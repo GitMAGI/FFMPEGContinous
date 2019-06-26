9b60ece4f689303dee8aa34dd01bfdaeea3929a6
Hikari Genzo <leonxp@libero.it>
2019-06-26 23:38:07 +0200

    Aggiornamento Utile

--- test00.py
+++ test00.py
@@ -3,16 +3,17 @@ import sys
 import os
 from nbstreamreader import NonBlockingStreamReader as NBSR
 import subprocess
-import select
 
 print("Starting ...")
 
 # INPUT PRE LOADING - BEGIN
 input_path = 'input'
 video_filename = 'video.mp4'
-input_fh = open(os.path.join(input_path, video_filename), 'rb')
-input_data = input_fh.read()
-input_fh.close()
+input_cmd = [ 'ffmpeg', '-i', os.path.join(input_path, video_filename), '-c:v', 'h264', '-f', 'h264', 'pipe:1' ]
+input_fh = subprocess.Popen(input_cmd, stdout = subprocess.PIPE)
+print("Command: '{}' started".format(" ".join(str(x) for x in input_cmd)))
+[input_data, input_err] = input_fh.communicate(input = input_fh)
+input_fh.kill()
 # INPUT PRE LOADING - END
 
 device_height = 1920
@@ -25,14 +26,12 @@ s_width = int(device_width * scaling_factor)
 write_output = True
 slice_size = 1024
 rgb_frame_size = s_width * s_height * 3
-unblocking_factor = 0.000001
+unblocking_factor = 0.008
 
-# C:\msys64\mingw64\bin\ffmpeg -i pipe:0 -c:v rawvideo -f rawvideo -an -pix_fmt rgb24 -s 192x108 pipe:1
+# C:\msys64\mingw64\bin\ffmpeg -i file.mp4 -c:v rawvideo -f rawvideo -an -pix_fmt rgb24 -s 192x108 pipe:1
 cmd = [
         'ffmpeg',
-        '-c:v', 'h264',
-        '-f', 'h264',
-        '-i', 'pipe:0',
+        '-i', os.path.join(input_path, video_filename),
         '-c:v', 'rawvideo',
         '-f', 'rawvideo',
         '-an','-sn',               # we want to disable audio processing (there is no audio)
@@ -42,11 +41,9 @@ cmd = [
     ]
 process = subprocess.Popen(    
     cmd,
-    stdin = subprocess.PIPE, 
     stdout = subprocess.PIPE
 )
 print("Command: '{}' started".format(" ".join(str(x) for x in cmd)))
-fin = process.stdin
 fout = NBSR(process.stdout, rgb_frame_size)
 
 frames = []
@@ -57,7 +54,6 @@ for i in range(1, len(input_data), slice_size):
     slice_data = input_data[start:stop]
 
     #print(slice_data)
-    fin.write(slice_data)
 
     try:
         decoded_frame = fout.read(unblocking_factor)
@@ -69,7 +65,9 @@ for i in range(1, len(input_data), slice_size):
         #print(str(e))
         pass
 
-    print("%d of %d" % (stop, len(input_data)))
+    #print("%d of %d" % (stop, len(input_data)))
+
+process.kill()
 
 print("Collected %d frames" % len(frames))
 print("Collected %d frames of byte arrays" % len(frames))
@@ -83,14 +81,14 @@ if write_output:
         output_fh = open(os.path.join(output_path, output_filename), 'ab')
         for frameBA in frameBAs:    
             output_fh.write(frameBA)
-        output_fh.close
+        output_fh.close()
     
     if frames is not None and len(frames) > 0:
         output_filename = 'data' + '_' + time.strftime("%Y%m%d-%H%M%S") + '.rgb24'
         output_fh = open(os.path.join(output_path, output_filename), 'ab')
         for frame in frames:
             output_fh.write(frame)
-        output_fh.close
+        output_fh.close()
 # OUTPUT POST WRITING - END
 
 print("Completed")
